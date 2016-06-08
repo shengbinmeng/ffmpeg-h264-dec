ee86195fc4313624345d57d4d82878910ea212fc
Shengbin Meng <shengbinmeng@gmail.com>
2016-06-08 17:53:19 +0800

    Add configure script of ffmpeg

---
+++ doc/config_h264dec.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#
+# read the configure help carefully before you want to change the following options
+#
+./configure \
+	--disable-asm \
+	--disable-pthreads \
+    --disable-doc \
+    --disable-encoders \
+    --disable-muxers \
+    --disable-decoders \
+    --enable-decoder=h264 \
+    --disable-parsers \
+    --enable-parser=h264 \
+    --disable-demuxers \
+    --enable-demuxer=h264 \
+    --disable-filters \
+    --disable-programs \
+    --enable-ffmpeg \
