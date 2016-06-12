d2b742565733a8ed04bfe756feff4a81880c9b35
Shengbin Meng <shengbinmeng@gmail.com>
2016-06-12 20:55:26 +0800

    Finish a program that may work for stream based codecs (but NOT H.264)

--- main.c
+++ main.c
@@ -2,57 +2,168 @@
 #include <stdlib.h>
 #include "libavcodec/avcodec.h"
 
+#define INBUF_SIZE 8192
+
+extern AVCodec ff_h264_decoder;
+
+static void yuv_save(unsigned char *buf[], int wrap[], int xsize,int ysize, const char *filename)
+{
+    FILE *f;
+    int i;	
+
+	f = fopen(filename, "w");
+	
+	for (i = 0; i < ysize; i++) {
+		fwrite(buf[0] + i * wrap[0], 1, xsize, f);
+	}
+
+	for (i = 0; i < ysize / 2; i++) {
+		fwrite(buf[1] + i * wrap[1], 1, xsize/2, f);
+	}
+	for (i = 0; i < ysize / 2; i++) {
+		fwrite(buf[2] + i * wrap[2], 1, xsize/2, f);
+	}
+
+    fclose(f);
+}
+
+
+static int decode_write_frame(const char *outfilename, AVCodecContext *avctx,
+                              AVFrame *frame, int *frame_count, AVPacket *pkt, int last)
+{
+    int len, got_frame;
+
+    len = avcodec_decode_video2(avctx, frame, &got_frame, pkt);
+    if (len < 0) {
+        fprintf(stderr, "Error while decoding frame %d\n", *frame_count);
+        return len;
+    }
+    printf("decode return %d\n", len);
+    if (got_frame) {
+        printf("Got frame %d\n", *frame_count);
+		if (outfilename) {
+			yuv_save(frame->data, frame->linesize, avctx->width, avctx->height, outfilename);
+		}
+		(*frame_count)++;
+    }
+    if (pkt->data) {
+        pkt->size -= len;
+        pkt->data += len;
+    }
+    return 0;
+}
+
+static void video_decode_example(const char *filename, const char *outfilename)
+{
+    AVCodec *codec;
+    AVCodecContext *c = NULL;
+    int frame_count;
+    FILE *f;
+    AVFrame *frame;
+    uint8_t inbuf[INBUF_SIZE + AV_INPUT_BUFFER_PADDING_SIZE];
+    AVPacket avpkt;
+
+    av_init_packet(&avpkt);
+
+    /* set end of buffer to 0 (this ensures that no overreading happens for damaged mpeg streams) */
+    memset(inbuf + INBUF_SIZE, 0, AV_INPUT_BUFFER_PADDING_SIZE);
+
+    printf("Decode video file %s to %s\n", filename, outfilename);
+
+    /* find the mpeg1 video decoder */
+    codec = avcodec_find_decoder(AV_CODEC_ID_H264);
+    if (!codec) {
+        fprintf(stderr, "Codec not found\n");
+        exit(1);
+    }
+
+    c = avcodec_alloc_context3(codec);
+    if (!c) {
+        fprintf(stderr, "Could not allocate video codec context\n");
+        exit(1);
+    }
+
+    if (codec->capabilities & AV_CODEC_CAP_TRUNCATED) {
+    	// we do not send complete frames
+    	printf("we do not send complete frames\n");
+        c->flags |= AV_CODEC_FLAG_TRUNCATED;
+    } else {
+    	printf("we must parse and send complete frames\n");
+    }
+
+    /* For some codecs, such as msmpeg4 and mpeg4, width and height
+       MUST be initialized there because this information is not
+       available in the bitstream. */
+
+    /* open it */
+    if (avcodec_open2(c, codec, NULL) < 0) {
+        fprintf(stderr, "Could not open codec\n");
+        exit(1);
+    }
+
+    f = fopen(filename, "rb");
+    if (!f) {
+        fprintf(stderr, "Could not open %s\n", filename);
+        exit(1);
+    }
+
+    frame = av_frame_alloc();
+    if (!frame) {
+        fprintf(stderr, "Could not allocate video frame\n");
+        exit(1);
+    }
+	
+    frame_count = 0;
+    for (;;) {
+        avpkt.size = fread(inbuf, 1, INBUF_SIZE, f);
+        printf("inbuf: %p, f: %p\n", inbuf, f);
+        if (avpkt.size == 0) {
+        	printf("Read nothing. feof: %d, ferror: %d\n", feof(f), ferror(f));
+            break;
+        }
+
+        /* NOTE1: some codecs are stream based (mpegvideo, mpegaudio)
+           and this is the only method to use them because you cannot
+           know the compressed data size before analysing it.
+
+           BUT some other codecs (msmpeg4, mpeg4) are inherently frame
+           based, so you must call them with all the data for one
+           frame exactly. You must also initialize 'width' and
+           'height' before initializing them. */
+
+        /* NOTE2: some codecs allow the raw parameters (frame size,
+           sample rate) to be changed at any frame. We handle this, so
+           you should also take care of it */
+
+        /* here, we use a stream based decoder (mpeg1video), so we
+           feed decoder and see if it could decode a frame */
+        avpkt.data = inbuf;
+        while (avpkt.size > 0)
+            if (decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 0) < 0) {
+            	fprintf(stderr, "decode or write error\n");
+                exit(1);
+            }
+    }
+
+    /* some codecs, such as MPEG, transmit the I and P frame with a
+       latency of one frame. You must do the following to have a
+       chance to get the last frame of the video */
+    avpkt.data = NULL;
+    avpkt.size = 0;
+    decode_write_frame(outfilename, c, frame, &frame_count, &avpkt, 1);
+
+    fclose(f);
+
+    avcodec_close(c);
+    av_free(c);
+    av_frame_free(&frame);
+    printf("Done\n");
+}
+
+
 int main()
 {
-	FILE *in_file = fopen("test/352x288Foreman.264", "rb");
-	if (!in_file) {
-		printf("Open input file error\n");
-		return -1;
-	}
-	
-	FILE *out_file = fopen("test/352x288Foreman.yuv", "wb");
-	if (!out_file) {
-		printf("Open output file error\n");
-	}
- 
- 	AVCodec *codec = avcodec_find_decoder(AV_CODEC_ID_H264);;
- 	if (!codec) {
-        fprintf(stderr, "codec not found\n");
-        exit(1);
-    }
-    struct AVCodecContext *c = avcodec_alloc_context3(codec); 
-	
-	if (avcodec_open2(c, codec, NULL) < 0) {
-        fprintf(stderr, "could not open codec\n");
-        exit(1);
-    }
-	
-    struct AVFrame *frame = av_frame_alloc();
-	if (!frame) {
-		return -1;
-	}
-
-	char* buf = (char*) calloc(1000000, sizeof(char));
-	int nal_len = 0;
-	int got_picture = 0;
-	while (!feof(in_file)) {
-		int consumed_bytes = h264_decode_frame(c, frame, &got_picture, buf, nal_len); 
-		if (consumed_bytes > 0) {
-			printf("consumed %d bytes\n", consumed_bytes);
-			if (!out_file) {
-				int i;
-				for (i=0; i<c->height; i++) {
-					fwrite(frame->data[0] + i * frame->linesize[0], 1, c->width, out_file);
-				}
-				for (i=0; i<c->height/2; i++) {
-					fwrite(frame->data[1] + i * frame->linesize[1], 1, c->width/2, out_file);
-				}
-				for (i=0; i<c->height/2; i++) {
-					fwrite(frame->data[2] + i * frame->linesize[2], 1, c->width/2, out_file);
-				}
-			}
-		}
-	}
-
+	avcodec_register(&ff_h264_decoder);
+	video_decode_example("test/352x288Foreman.264", "test/352x288Foreman.yuv");
 	return 0;		
 }
